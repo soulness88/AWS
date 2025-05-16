d0590f741393cc1aac5cf21f9fdd536622608e09
soulness88 <106523827+soulness88@users.noreply.github.com>
2025-05-16 11:23:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
         <h1 align="center">Buenos dias, estudiantes</h1>
         <br>
         <div align="center">
-            <img src="playa2.jph">
+            <img src="playa2.jpg">
         </div>
     </body>
 </head>
+</html>
