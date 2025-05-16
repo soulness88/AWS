b2ef94fe04df1f90ced5b742acbc38134f996a40
soulness88 <106523827+soulness88@users.noreply.github.com>
2025-05-16 11:18:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Redait Media</title>
+    <body>
+        <h1 align="center">Buenos dias, estudiantes</h1>
+        <br>
+        <div align="center">
+            <img src="playa2.jph">
+        </div>
+    </body>
+</head>
